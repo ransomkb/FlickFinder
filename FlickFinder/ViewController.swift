eb13442a5f62e39bdd52bf31ef2543951e441ac0
Ransom Barber <ransomkb@yahoo.com>
2015-05-04 18:49:48 +0900

    Initial Commit

---
+++ FlickFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickFinder
+//
+//  Created by Ransom Barber on 5/4/15.
+//  Copyright (c) 2015 Hart Book. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
